86bdaa8b510f260f1116762be79ec85d2d5c33b5
ThinhNguyen1999 <nguyencducthinh@gmail.com>
2025-02-17 08:33:47 +0700

    Create project

---
+++ Source/AirHockey/AirHockey.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class AirHockey : ModuleRules
+{
+	public AirHockey(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+    }
+}
---
+++ Source/AirHockey/AirHockey.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AirHockey.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AirHockey, "AirHockey" );
+
+DEFINE_LOG_CATEGORY(LogAirHockey)
+ 
