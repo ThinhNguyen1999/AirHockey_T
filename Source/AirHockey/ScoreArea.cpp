ede76beaff4c92fefb5e300c262967a4dfd4b6cc
ThinhNguyen1999 <nguyencducthinh@gmail.com>
2025-02-25 17:01:54 +0700

    ScoreArea

---
+++ Source/AirHockey/ScoreArea.cpp
@@ -0,0 +1,34 @@
+﻿
+
+
+#include "ScoreArea.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+AScoreArea::AScoreArea()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AreaMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Area Mesh"));
+	SetRootComponent(AreaMesh);
+
+	AreaCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("Wall Collision"));
+	AreaCollision->SetupAttachment(AreaMesh);
+}
+
+// Called when the game starts or when spawned
+void AScoreArea::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AScoreArea::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/AirHockey/ScoreArea.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ScoreArea.generated.h"
+
+UCLASS()
+class AIRHOCKEY_API AScoreArea : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AScoreArea();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UBoxComponent* AreaCollision;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UStaticMeshComponent* AreaMesh;
+};
