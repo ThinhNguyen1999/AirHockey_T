85d3d5548bffc9e6ca6971de87fc7c86049b102b
ThinhNguyen1999 <nguyencducthinh@gmail.com>
2025-02-25 16:30:59 +0700

    Create project C++

---
+++ Source/AirHockey/Ball.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ball.h"
+
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+ABall::ABall()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	BallCollision = CreateDefaultSubobject<USphereComponent>(TEXT("BallCollision"));
+	BallCollision->SetupAttachment(BallMesh);
+
+	BallMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BallMesh"));
+		SetRootComponent(BallMesh);
+}
+ 
+// Called when the game starts or when spawned
+void ABall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/AirHockey/Wall.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Wall.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+AWall::AWall()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	WallMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Wall Mesh"));
+	SetRootComponent(WallMesh);
+
+	WallCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("Wall Collision"));
+	WallCollision->SetupAttachment(WallMesh);
+
+	
+}
+
+// Called when the game starts or when spawned
+void AWall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
