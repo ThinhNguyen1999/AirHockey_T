2ae0738b1ea6de1858509adfa7f9a637a15b86d6
ThinhNguyen1999 <nguyencducthinh@gmail.com>
2025-02-28 16:17:54 +0700

    Update

---
+++ Source/AirHockey/Handle.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Handle.h"
+
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+AHandle::AHandle()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	HandleMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HandleMesh"));
+	SetRootComponent(HandleMesh);
+
+	HandleCollision = CreateDefaultSubobject<USphereComponent>("HandleCollision");
+	HandleCollision->SetupAttachment(HandleMesh);
+	
+}
+
+// Called when the game starts or when spawned
+void AHandle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHandle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
